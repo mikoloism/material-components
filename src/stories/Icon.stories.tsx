c95b2b7b3cf6273204ae04f8fec18611299f39e4
mikoloism <mikoloism@gmail.com>
2023-01-22 16:38:10 +0330

    perf(stories): add `argTypes` to `icon` stories component;
    support icon enums to use as `storybook` selection argType option;

--- src/stories/Icon.stories.tsx
+++ src/stories/Icon.stories.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import '../styles/globals.css';
 import 'material-symbols/index.css';
-import { Icon } from '../components/Icon';
+import { Icon, IconSymbol } from '../components/Icon';
+import { Grade, IconKind, OpticalSize, Weight } from '../components/Icon/types';
+import { EnumDeclaration, EnumMember, EnumType } from 'typescript';
+
+function adapterEnumToArgType(Enumerable: object, defaultValue?: string): any {
+	return {
+		defaultValue,
+		options: Object.values(Enumerable).filter((x) => typeof x === 'string'),
+		mapping: Enumerable,
+		control: { type: 'select' },
+	};
+}
 
 export default {
 	title: 'Example/Icon',
 	component: Icon,
+	argTypes: {
+		icon: adapterEnumToArgType(IconSymbol),
+		kind: adapterEnumToArgType(IconKind, 'Rounded'),
+		isFill: { type: 'boolean', defaultValue: undefined },
+		grade: adapterEnumToArgType(Grade, 'Middle'),
+		weight: adapterEnumToArgType(Weight, 'Regular'),
+		opticalSize: adapterEnumToArgType(OpticalSize, 'Medium'),
+	},
 } as ComponentMeta<typeof Icon>;
 
 const Template: ComponentStory<typeof Icon> = function Template(args: any) {
