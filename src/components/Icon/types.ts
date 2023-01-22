2c34d917969cea968d6afab5cfec5896d7434029
mikoloism <mikoloism.github@gmail.com>
2023-01-22 11:31:45 +0330

    fix(component): re-export named `IconSymbol` enum from `IconName` to `IconSymbol`;

--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -1,10 +1,11 @@
+export { IconSymbol, type IconSymbolType } from './symbol.enum';
+
 export enum IconKind {
 	Outline = 'outlined',
 	Rounded = 'rounded',
 	Sharp = 'sharp',
 }
 export type IconKindType = `${IconKind}` | IconKind;
-export { IconSymbol, type IconSymbolType } from './symbol.enum';
 
 export enum OpticalSize {
 	Small = 20,
